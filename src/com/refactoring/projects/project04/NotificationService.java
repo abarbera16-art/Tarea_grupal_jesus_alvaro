c9a6ae502751a10fa42912910c7956a31c1b8b24
Jesús <j32u207@gmail.com>
2026-03-01 13:13:18 +0100

    Refactorizacion Paso 1.3: Extraer constante llamada Push para eliminar el dato Hardcodeado

--- src/com/refactoring/projects/project04/NotificationService.java
+++ src/com/refactoring/projects/project04/NotificationService.java
@@ -1,17 +1,18 @@
 package com.refactoring.projects.project04;
 
 public class NotificationService {
+
 	//Aqui extraemos las constantes
     private static final String TIPO_EMAIL = "email";
     private static final String TIPO_SMS = "SMS";
-
+	private static final String TIPO_PUSH = "push";
     
     public void enviarNotificacion(String tipo, String mensaje, String destinatario) {
         if (TIPO_EMAIL.equals(tipo)) {
             System.out.println("Enviando email a " + destinatario + ": " + mensaje);
         } else if (tipo.equals(TIPO_SMS)) {
             System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals("push")) {
+        } else if (tipo.equals(TIPO_PUSH)) {
             System.out.println("Enviando push a " + destinatario + ": " + mensaje);
         }
     }
