110b247d23d3c4092a206715905d0b5311d1cc20
Jesús <j32u207@gmail.com>
2026-03-01 13:10:20 +0100

    Refactorizacion Paso 1.2: Extraer constante llamada SMS para eliminar el dato Hardcodeado

--- src/com/refactoring/projects/project04/NotificationService.java
+++ src/com/refactoring/projects/project04/NotificationService.java
@@ -1,15 +1,15 @@
 package com.refactoring.projects.project04;
 
 public class NotificationService {
-    //Aqui extraemos las constantes
+	//Aqui extraemos las constantes
     private static final String TIPO_EMAIL = "email";
-
+    private static final String TIPO_SMS = "SMS";
 
     
     public void enviarNotificacion(String tipo, String mensaje, String destinatario) {
         if (TIPO_EMAIL.equals(tipo)) {
             System.out.println("Enviando email a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals("sms")) {
+        } else if (tipo.equals(TIPO_SMS)) {
             System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
         } else if (tipo.equals("push")) {
             System.out.println("Enviando push a " + destinatario + ": " + mensaje);
