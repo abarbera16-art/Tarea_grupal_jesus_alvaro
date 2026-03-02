901eaffeeaf4c3840941a21db4ad921b20136023
Jesús <j32u207@gmail.com>
2026-03-02 12:26:17 +0100

    Docs 3.1: Añadir JavaDoc a la interfaz Notificacion.

--- src/com/refactoring/projects/project04/Notificacion.java
+++ src/com/refactoring/projects/project04/Notificacion.java
@@ -1,5 +1,18 @@
 package com.refactoring.projects.project04;
 
+/**
+ * Interfaz común para todos los tipos de notificaciones.
+ * <p>
+ * Aplica el principio de inversión de dependencias para permitir polimorfismo.
+ * </p>
+ * @author Jesus y Alvaro
+ * @version 1.0
+ */
 public interface Notificacion {
-void enviar(String mensaje, String destinatario);
+    /**
+     * Envía el mensaje al destinatario especificado.
+     * @param mensaje El contenido a enviar.
+     * @param destinatario El receptor.
+     */
+    void enviar(String mensaje, String destinatario);
 }
