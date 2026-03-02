b5a11a8c3a80098cf63dfc4cf123b0ea87962ba5
Jesús <j32u207@gmail.com>
2026-03-02 12:51:37 +0100

    Docs 3.4: Actualizamos el package-info con el tercer paso de Refactorizacion todo resumido.

--- src/com/refactoring/projects/project04/package-info.java
+++ src/com/refactoring/projects/project04/package-info.java
@@ -1,14 +1,14 @@
 /**
  * Paquete que contiene la lógica del sistema de notificaciones.
  * <p>
- * En este paquete se aplican técnicas de refactorización para mejorar
- * la calidad del software, incluyendo:
+ * En este paquete se han aplicado tres niveles de refactorización :
  * <ul>
- * <li>Extracción de Constantes (Paso 1)</li>
- * <li>Extracción de Métodos (Paso 2)</li>
+ * <li><b>Paso 1:</b> Extracción de Constantes.</li>
+ * <li><b>Paso 2:</b> Extracción de Métodos para reducir la complejidad ciclomática.</li>
+ * <li><b>Paso 3:</b> Reemplazo de condicionales por Polimorfismo.</li>
  * </ul>
  * </p>
  * @author Jesus y Alvaro
- * @version 1.0
+ * @version 1.3
  */
 package com.refactoring.projects.project04;
