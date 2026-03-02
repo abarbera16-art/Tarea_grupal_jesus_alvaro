780ad57faf22a3d962d0474d3a50a5d00f2f3700
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-03 00:33:31 +0100

    feat: añadir implementaciones concretas para notificaciones en Email,SMS y Push

---
+++ src/com/refactoring/projects/project04/SmsNotificacion.java
@@ -0,0 +1,8 @@
+package com.refactoring.projects.project04;
+
+public class SmsNotificacion implements NotificacionStrategy {
+    @Override
+    public void enviar(String mensaje, String destinatario) {
+        System.out.println("Enviando Sms a " + destinatario + ": " + mensaje);
+    }
+}
