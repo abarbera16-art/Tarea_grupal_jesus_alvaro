51e8170298d490cafb3ca4dc7d27fd6f2a45ffaf
Jesús <j32u207@gmail.com>
2026-03-02 12:39:31 +0100

    Docs 3.2.2: Añadimos los comentarios de la clase SMSNotificacion.

--- src/com/refactoring/projects/project04/SMSNotificacion.java
+++ src/com/refactoring/projects/project04/SMSNotificacion.java
@@ -1,6 +1,17 @@
 package com.refactoring.projects.project04;
 
+/**
+ * Implementación de la estrategia de notificación por SMS.
+ * * @author Jesus y Alvaro
+ * @version 1.0
+ */
 public class SMSNotificacion implements Notificacion {
+
+    /**
+     * Envía un mensaje de texto corto (SMS).
+     * @param mensaje El texto del SMS.
+     * @param destinatario El número de teléfono.
+     */
     @Override
     public void enviar(String mensaje, String destinatario) {
         System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
