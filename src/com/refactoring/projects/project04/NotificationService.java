4d78ecdeafd2407c72efcf1a7ce26cdee65ef388
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-03 00:32:54 +0100

    feat: definir interfaz NotificacionStrategy para aplicar el polimorfismo

--- src/com/refactoring/projects/project04/NotificationService.java
+++ src/com/refactoring/projects/project04/NotificationService.java
@@ -2,18 +2,7 @@ package com.refactoring.projects.project04;
 
 public class NotificationService {
 
-	private static final String SMS = "sms";
-	private static final String PUSH = "push";
-	private static final String EMAIL = "email";
-
-	public void enviarNotificacion(String tipo, String mensaje, String destinatario) {
-        if (tipo.equals(EMAIL)) {
-            System.out.println("Enviando email a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals(SMS)) {
-            System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
-        } else if (tipo.equals(PUSH)) {
-            System.out.println("Enviando push a " + destinatario + ": " + mensaje);
-        }
+    public void enviarNotificacion(NotificacionStrategy estrategia, String mensaje, String destinatario) {
+        estrategia.enviar(mensaje, destinatario);
     }
-	
 }
