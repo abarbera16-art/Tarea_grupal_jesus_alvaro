5507d689384e80640195c602fab23a8d0b6a630d
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-03 01:52:52 +0100

    refactor: aplicar Extract Method para organizar los tipos de notificación

--- src/com/refactoring/projects/project04/NotificationService.java
+++ src/com/refactoring/projects/project04/NotificationService.java
@@ -1,9 +1,30 @@
 package com.refactoring.projects.project04;
 
 public class NotificationService {
+    
+    private static final String SMS = "sms";
+    private static final String PUSH = "push";
+    private static final String EMAIL = "email";
 
-    public void enviarNotificacion(NotificacionStrategy estrategia, String mensaje, String destinatario) {
-        estrategia.enviar(mensaje, destinatario);
-        
+    public void enviarNotificacion(String tipo, String mensaje, String destinatario) {
+        if (tipo.equals(EMAIL)) {
+            enviarEmail(mensaje, destinatario);
+        } else if (tipo.equals(SMS)) {
+            enviarSMS(mensaje, destinatario);
+        } else if (tipo.equals(PUSH)) {
+            Enviarpush(mensaje, destinatario);
+        }
     }
+
+	private void Enviarpush(String mensaje, String destinatario) {
+		System.out.println("Enviando push a " + destinatario + ": " + mensaje);
+	}
+
+	private void enviarSMS(String mensaje, String destinatario) {
+		System.out.println("Enviando SMS a " + destinatario + ": " + mensaje);
+	}
+
+	private void enviarEmail(String mensaje, String destinatario) {
+		System.out.println("Enviando email a " + destinatario + ": " + mensaje);
+	}
 }
