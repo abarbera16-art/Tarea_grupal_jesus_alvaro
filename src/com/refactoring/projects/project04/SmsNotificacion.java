454c9ba7e035d63f7c80dfba2b722b484121f8fd
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-04 13:18:16 +0100

    refactor: SmsNotificacion: Clase concreta que aísla la lógica específica para enviar mensajes SMS.

--- src/com/refactoring/projects/project04/SmsNotificacion.java
+++ src/com/refactoring/projects/project04/SmsNotificacion.java
@@ -4,5 +4,5 @@ public class SmsNotificacion implements NotificacionStrategy {
     @Override
     public void enviar(String mensaje, String destinatario) {
         System.out.println("Enviando Sms a " + destinatario + ": " + mensaje);  
-    }
+     }
 }
