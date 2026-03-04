f0055025371bdf2ed646c7a8d61b00055657d638
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-04 16:50:09 +0100

    nueva version del codigo para aumentar la compabilidada  al ahora de mergear

--- src/com/refactoring/projects/project04/PushNotificacion.java
+++ src/com/refactoring/projects/project04/PushNotificacion.java
@@ -1,8 +1,19 @@
 package com.refactoring.projects.project04;
 
+/**
+ * Implementación de la estrategia de notificación Push.
+ * * @author Jesus y Alvaro
+ * @version 1.0
+ */
 public class PushNotificacion implements NotificacionStrategy {
+
+    /**
+     * Envía una notificación Push a un dispositivo.
+     * @param mensaje El contenido de la notificación.
+     * @param destinatario El ID del dispositivo o usuario.
+     */
     @Override
     public void enviar(String mensaje, String destinatario) {
-        System.out.println("Enviando push a " + destinatario + ": " + mensaje);  
+        System.out.println("Enviando push a " + destinatario + ": " + mensaje);
     }
 }
