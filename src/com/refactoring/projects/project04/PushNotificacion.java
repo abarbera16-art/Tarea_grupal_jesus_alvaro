bfdf1711c45dbad23558a6a928db063cd09a993e
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-04 13:20:19 +0100

    refactor: PushNotificacion: Clase concreta que aísla la lógica específica para enviar notificaciones push.

--- src/com/refactoring/projects/project04/PushNotificacion.java
+++ src/com/refactoring/projects/project04/PushNotificacion.java
@@ -3,6 +3,6 @@ package com.refactoring.projects.project04;
 public class PushNotificacion implements NotificacionStrategy {
     @Override
     public void enviar(String mensaje, String destinatario) {
-        System.out.println("Enviando push a " + destinatario + ": " + mensaje); 
+        System.out.println("Enviando push a " + destinatario + ": " + mensaje);  
     }
 }
