1f7cbea8629e6a6ae9e43d04cbd869d16f178ca5
TuNombreDeUsuario <abarbera16@gmail.com>
2026-03-04 13:20:58 +0100

    refactor: NotificationService: Clase principal refactorizada. Se eliminan los condicionales (if/else) y ahora delega la ejecución a la estrategia que reciba por parámetro (Polimorfismo).

--- src/com/refactoring/projects/project04/NotificationService.java
+++ src/com/refactoring/projects/project04/NotificationService.java
@@ -2,7 +2,7 @@ package com.refactoring.projects.project04;
 
 public class NotificationService {
 
-    public void enviarNotificacion(NotificacionStrategy estrategia, String mensaje, String destinatario) {
+    public void enviarNotificacion(NotificacionStrategy estrategia, String mensaje, String destinatario) { 
         estrategia.enviar(mensaje, destinatario);
     }
 }
